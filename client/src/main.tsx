4ebf7daae4e49004790475da43ef22f9354102ea
Karl Fors <kwfors1@gmail.com>
2025-12-15 21:04:22 -0500

    toast

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx"
 import { BrowserRouter } from "react-router-dom"
 import { AuthProvider } from "./context/AuthContext"
 import { HelmetProvider } from "react-helmet-async"
+import { Toaster } from "react-hot-toast"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -13,6 +14,20 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <AuthProvider>
           <App />
+
+          {/* ✅ Global toast container */}
+          <Toaster
+            position="top-center"
+            containerStyle={{ top: 80 }} // avoids navbar overlap
+            toastOptions={{
+              duration: 4000,
+              style: {
+                background: "#111827",
+                color: "#ffffff",
+                borderRadius: "12px",
+              },
+            }}
+          />
         </AuthProvider>
       </BrowserRouter>
     </HelmetProvider>
